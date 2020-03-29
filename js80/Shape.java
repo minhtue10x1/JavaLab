54c4579399182b29745408f10107a8e6730d645d
minhtue10x1 <61979098+minhtue10x1@users.noreply.github.com>
2020-03-29 11:36:15 +0700

    Add files via upload

---
+++ js80/Shape.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab80;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Shape {
+
+    public Shape() {
+    }
+    
+    public double getArea(){
+        return 0;
+    }
+    
+    public double getVolume(){
+        return 0;
+    }
+}
